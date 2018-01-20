70df03853543aa0fe25c298deb551331ffc40657
infarkt <9221932@mail.ru>
2018-01-20 15:54:23 +0300

    export form-elem to component

--- front/block/staff/component/form.js
+++ front/block/staff/component/form.js
@@ -2,31 +2,12 @@ import React from 'react';
 import { observer, inject } from 'mobx-react';
 
 import { Form, 
-            Icon, 
-            Input,
             Modal,
-            Radio,
             Alert,
-            DatePicker,
-            Checkbox,
             Row,
-            Col,
-            InputNumber } from 'antd';
+            Col } from 'antd';
 
-const RadioButton = Radio.Button;
-const RadioGroup = Radio.Group;
-const { TextArea } = Input;
-
-import Dictionary from '../../../common/element/dictionary'
-
-const formItemLayout = {
-    labelCol: {
-        sm: { span: 8 }
-    },
-    wrapperCol: {
-        sm: { span: 16 }
-    },
-};
+import FormElem from '../../../common/element/form_elem'
 
 
 @inject("staffStore") @observer
@@ -46,6 +27,8 @@ class ModalForm extends React.Component {
 	}
 	render() {
         const { scheme, form, model } = this.props.staffStore
+
+        console.log('update')
         
 		return (
 			<Modal
@@ -61,16 +44,7 @@ class ModalForm extends React.Component {
                                 return <Col span={12} key={column}>
                                     {Object.keys(scheme).map(key => {
                                         if(key != 'store' && scheme[key].options.col == column) {
-                                            return <Form.Item key={key} label={scheme[key].options.name} {...formItemLayout}> 
-                                                        {{'Input': <Input {...scheme[key].elem}/>,
-                                                            'Date': <DatePicker {...scheme[key].elem}/>,
-                                                            'Checked': <Checkbox {...scheme[key].elem}/>,
-                                                            'Dictionary': <Dictionary data={scheme[key].elem}/>,
-                                                            'Number': <InputNumber {...scheme[key].elem}/>,
-                                                            'Textarea': <TextArea {...scheme[key].elem}/>,
-                                                        }[scheme[key].options.type]}
-                                                    </Form.Item>
-                
+                                            return <FormElem key={key} data={scheme[key]} />
                                         }
                                     })}
                                 </Col>
@@ -87,30 +61,3 @@ class ModalForm extends React.Component {
 }
 
 export default ModalForm
-
-
-// {modalScheme != '' ? [...Array(scheme.order_scheme.main_form.settings.column)].map((num, column) => {
-//     return <Col span={12} key={column}>
-//                 {Object.keys(modalScheme).map((item, index) => {
-//                     if(modalScheme[item].options.column == column) {
-//                         return <FormElement key={index} data={modalScheme[item]}/>
-//                     }
-//                 })}
-//             </Col>
-// }) : ''}
-
-
-// <Form.Item label="Компания" {...formItemLayout}>
-// <SearchCompany data={{
-//     value: model.company,
-//     setValue: (value) => this.setModelValue('company', value),
-//     setSelect: (id, value) => this.setSelect('company', id, value)
-// }}/>
-// </Form.Item>
-
-
-// <Form.Item label="Справоник" {...formItemLayout}>
-// <Dictionary data={{group: 3,
-//                     value: model.source,
-//                     setValue: (value) => this.setModelValue('source', value)}}/>
-// </Form.Item>
---
+++ front/common/element/form_elem.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { observer, inject } from 'mobx-react';
+
+import { Form, 
+            Icon, 
+            Input,
+            Modal,
+            Radio,
+            Alert,
+            DatePicker,
+            Checkbox,
+            Row,
+            Col,
+            InputNumber } from 'antd';
+
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
+const { TextArea } = Input;
+
+import Dictionary from './dictionary'
+
+const formItemLayout = {
+    labelCol: {
+        sm: { span: 8 }
+    },
+    wrapperCol: {
+        sm: { span: 16 }
+    },
+};
+
+@inject("staffStore") @observer
+class FormElem extends React.Component {
+    constructor(props) {
+        super(props);
+
+
+    }
+    componentWillReceiveProps(nextProps) {
+        
+	}
+    componentWillMount() {
+        
+    }
+    render() {
+        const { data } = this.props
+
+        return (
+            <Form.Item label={data.options.name} {...formItemLayout}> 
+                {{'Input': <Input {...data.elem}/>,
+                    'Date': <DatePicker {...data.elem}/>,
+                    'Checked': <Checkbox {...data.elem}/>,
+                    'Dictionary': <Dictionary data={data.elem}/>,
+                    'Number': <InputNumber {...data.elem}/>,
+                    'Textarea': <TextArea {...data.elem}/>,
+                }[data.options.type]}
+            </Form.Item>
+        );
+    }
+}
+
+export default FormElem
+
+
